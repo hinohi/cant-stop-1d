0788dd13c8d797ad0c010dfe33aa31969a93d66a
Nakayama Daiju <42.daiju@gmail.com>
2021-01-26 02:26:16 +0900

    clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Clap;
+
+#[derive(Debug, Clap)]
+struct Opts {
+    #[clap(long, default_value = "1")]
+    dice_min: usize,
+    #[clap(short, long, default_value = "6")]
+    dice_max: usize,
+    #[clap(short, long)]
+    goal: usize,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+    println!("{:?}", opts);
 }
