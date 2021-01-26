478aaa1068db9b18613dd0d3a1c2d4b39b9b477f
Nakayama Daiju <42.daiju@gmail.com>
2021-01-26 23:01:23 +0900

    できた

---
+++ src/expr.rs
@@ -0,0 +1,200 @@
+use std::ops::{Add, Div, Mul};
+
+use Expr::*;
+
+#[derive(Debug, Clone)]
+pub enum Expr {
+    Sum {
+        one: f64,
+        zero: f64,
+        nonlinear: Vec<Expr>,
+    },
+    Min {
+        a: Box<Expr>,
+        b: Box<Expr>,
+    },
+}
+
+impl Expr {
+    pub const fn linear(x: f64, a: f64) -> Expr {
+        Sum {
+            one: x,
+            zero: a,
+            nonlinear: Vec::new(),
+        }
+    }
+
+    pub fn min(self, other: Expr) -> Expr {
+        Min {
+            a: Box::new(self),
+            b: Box::new(other),
+        }
+    }
+
+    pub fn eval(&self, x: f64) -> f64 {
+        match self {
+            Sum {
+                one,
+                zero,
+                nonlinear,
+            } => *one * x + *zero + nonlinear.iter().map(|e| e.eval(x)).sum::<f64>(),
+            Min { a, b } => {
+                let a = a.eval(x);
+                let b = b.eval(x);
+                if a <= b {
+                    a
+                } else {
+                    b
+                }
+            }
+        }
+    }
+
+    pub fn bisect(&self) -> f64 {
+        let mut lo = 0.5;
+        while self.eval(lo) < lo {
+            lo /= 2.0
+        }
+        let mut hi = 1.0;
+        while hi < self.eval(hi) {
+            hi *= 2.0;
+        }
+        while hi - lo > 1e-12 {
+            let mid = (hi + lo) / 2.0;
+            let v = self.eval(mid);
+            if v < mid {
+                hi = mid;
+            } else {
+                lo = mid;
+            }
+        }
+        hi
+    }
+}
+
+impl Add<Expr> for Expr {
+    type Output = Expr;
+    fn add(self, rhs: Expr) -> Self::Output {
+        match self {
+            Sum {
+                one,
+                zero,
+                mut nonlinear,
+            } => match rhs {
+                Sum {
+                    one: r_one,
+                    zero: r_zero,
+                    nonlinear: mut r_nonlinear,
+                } => {
+                    nonlinear.append(&mut r_nonlinear);
+                    Sum {
+                        one: one + r_one,
+                        zero: zero + r_zero,
+                        nonlinear,
+                    }
+                }
+                e => {
+                    nonlinear.push(e);
+                    Sum {
+                        one,
+                        zero,
+                        nonlinear,
+                    }
+                }
+            },
+            e => match rhs {
+                Sum {
+                    one,
+                    zero,
+                    mut nonlinear,
+                } => {
+                    nonlinear.push(e);
+                    Sum {
+                        one,
+                        zero,
+                        nonlinear,
+                    }
+                }
+                ee => Sum {
+                    one: 0.0,
+                    zero: 0.0,
+                    nonlinear: vec![e, ee],
+                },
+            },
+        }
+    }
+}
+
+impl Mul<f64> for Expr {
+    type Output = Expr;
+    fn mul(self, rhs: f64) -> Expr {
+        match self {
+            Sum {
+                one,
+                zero,
+                nonlinear,
+            } => Sum {
+                one: one * rhs,
+                zero: zero * rhs,
+                nonlinear: nonlinear.into_iter().map(|e| e * rhs).collect(),
+            },
+            Min { a, b } => Min {
+                a: Box::new(a.mul(rhs)),
+                b: Box::new(b.mul(rhs)),
+            },
+        }
+    }
+}
+
+impl Div<f64> for Expr {
+    type Output = Expr;
+    fn div(self, rhs: f64) -> Expr {
+        match self {
+            Sum {
+                one,
+                zero,
+                nonlinear,
+            } => Sum {
+                one: one / rhs,
+                zero: zero / rhs,
+                nonlinear: nonlinear.into_iter().map(|e| e / rhs).collect(),
+            },
+            Min { a, b } => Min {
+                a: Box::new(a.div(rhs)),
+                b: Box::new(b.div(rhs)),
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn eval_linear() {
+        assert_eq!(Expr::linear(0.0, 3.14).eval(-3.0), 3.14);
+        assert_eq!(Expr::linear(1.0, 0.0).eval(3.0), 3.0);
+        assert_eq!(Expr::linear(1.5, 1.0).eval(10.0), 16.0);
+        assert_eq!(Expr::linear(-0.5, 2.0).eval(3.0), 0.5);
+    }
+
+    #[test]
+    fn eval_min() {
+        let m = Expr::linear(2.0, -1.0).min(Expr::linear(-0.5, 4.0));
+        assert_eq!(m.eval(0.0), -1.0);
+        assert_eq!(m.eval(1.0), 1.0);
+        assert_eq!(m.eval(2.0), 3.0);
+        assert_eq!(m.eval(3.0), 2.5);
+    }
+
+    #[test]
+    fn bisect() {
+        let f = Expr::linear(0.5, 0.5).min(Expr::linear(0.0, 2.0));
+        let g = Expr::linear(0.4, 1.0).min(Expr::linear(0.0, 3.0));
+        let h = Expr::linear(0.6, 2.0).min(Expr::linear(0.0, 1.5));
+        let e = (f + g + h) / 3.0;
+        let x = e.bisect();
+        assert!((e.eval(x) - x).abs() < 1e-6);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,118 @@
+mod expr;
+
+use std::collections::HashMap;
+
 use clap::Clap;
+
+use crate::expr::Expr;
 
 #[derive(Debug, Clap)]
 struct Opts {
-    #[clap(long, default_value = "1")]
-    dice_min: usize,
     #[clap(short, long, default_value = "6")]
-    dice_max: usize,
-    #[clap(short, long)]
-    goal: usize,
+    dice: u32,
+    #[clap(short, long, default_value = "20")]
+    goal: u32,
+}
+
+#[derive(Debug, Eq, PartialEq, Hash)]
+struct State {
+    original_pos: u32,
+    pos: u32,
+    dice: Option<u32>,
+}
+
+impl State {
+    pub const fn new(original_pos: u32) -> State {
+        State {
+            original_pos,
+            pos: original_pos,
+            dice: None,
+        }
+    }
+
+    pub const fn dice(&self, dice: u32) -> State {
+        State {
+            original_pos: self.original_pos,
+            pos: self.original_pos + dice,
+            dice: Some(dice),
+        }
+    }
+
+    pub fn go_success(&self) -> State {
+        State {
+            original_pos: self.original_pos,
+            pos: self.pos + self.dice.unwrap(),
+            dice: self.dice,
+        }
+    }
+
+    pub const fn stop(&self) -> State {
+        State {
+            original_pos: self.pos,
+            pos: self.pos,
+            dice: None,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Solver {
+    opts: Opts,
+    mem: HashMap<u32, f64>,
+}
+
+impl Solver {
+    pub fn new(opts: Opts) -> Solver {
+        let n = (opts.goal + 1) * (opts.goal + 1) * opts.dice;
+        Solver {
+            opts,
+            mem: HashMap::with_capacity(n as usize),
+        }
+    }
+
+    pub fn solve(&mut self, n: u32) -> f64 {
+        self.dfs_a(State::new(n))
+    }
+
+    const fn dice_n(&self) -> f64 {
+        self.opts.dice as f64
+    }
+
+    fn dfs_a(&mut self, state: State) -> f64 {
+        assert!(state.dice.is_none());
+        if state.pos >= self.opts.goal {
+            return 0.0;
+        }
+        if let Some(result) = self.mem.get(&state.pos) {
+            return result.clone();
+        }
+        let mut s = Expr::linear(0.0, 1.0);
+        for dice in 1..=self.opts.dice {
+            s = s + self.dfs_b(state.dice(dice)) / self.dice_n();
+        }
+        let r = s.bisect();
+        self.mem.insert(state.pos, r.clone());
+        r
+    }
+
+    fn dfs_b(&mut self, state: State) -> Expr {
+        assert!(state.dice.is_some());
+        if state.pos >= self.opts.goal {
+            return Expr::linear(0.0, 0.0);
+        }
+        let stop = self.dfs_a(state.stop());
+        let go_success = self.dfs_b(state.go_success());
+        let go_fail = Expr::linear(1.0, 1.0);
+        let go = go_success / self.dice_n() + go_fail * ((self.dice_n() - 1.0) / self.dice_n());
+        go.min(Expr::linear(0.0, stop))
+    }
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
-    println!("{:?}", opts);
+    let goal = opts.goal;
+    let mut solver = Solver::new(opts);
+    for i in 0..goal {
+        println!("{} {}", i, solver.solve(i));
+    }
 }
